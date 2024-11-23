bc07f7ce963a96168f6c5dff6523a9fdc5cea8f2
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2024-11-23 17:01:33 -0300

    Add files via upload

---
+++ aula_5.py
@@ -0,0 +1,40 @@
+# 1 - instalar o python
+# 2 - instalar o vscode
+# 3 - instalar a extensão
+# JUNTAR ==  CONCATENAR 
+
+
+# int(), float(), str()
+
+n = 2.50
+print(str(n))
+
+
+# nome  = input('Digite seu nome: ')
+
+# print('SEJA BEM VINDO(A)',nome)
+# print('SEJA BEM VINDO(A)'+ '' + nome)
+# print(f'SEJA BEM VINDO(A) {nome}')
+# print('SEJA BEM VINDO(A) {} '.format(nome))
+# print('SEJA BEM VINDO(A) %s '%(nome))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# por padrão o input é um texto 
+# numero1 = input('Digite um número: ')
+# numero2 = input('Digite um número: ')
+# soma  =  numero1 + numero2
+# # tipo  =  type(numero1)
+# print(soma)
